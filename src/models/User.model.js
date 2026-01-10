ff2d747e645a4f5e7aae7ae3cc9065dda082fed3
Abhishek Mishra <akmakm10074@gmail.com>
2026-01-10 20:45:24 +0530

    add USer Model with secure password hashing

---
+++ src/models/User.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6,
+        select: false //password never comes in queries unless explicitly asked
+    }
+}, { timestamps: true });
+
+userSchema.pre("save", async function (next) {
+
+    if (!this.isModified("password")) return next();
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+const User = mongoose.model("User", userSchema);
+export default User;
