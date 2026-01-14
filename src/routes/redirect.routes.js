c8507d0a09f460a2b602a33d43d3d58ff7f3fd2a
Abhishek Mishra <akmakm10074@gmail.com>
2026-01-14 21:07:46 +0530

    added limit for url creation and redirect per user/IP

--- src/routes/redirect.routes.js
+++ src/routes/redirect.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import {redirectShortUrl} from "../controllers/url.controller.js";
+import { redirectShortUrl } from "../controllers/url.controller.js";
+import { redirectLimiter } from "../middleware/rateLimit.middleware.js"
 
 const router = express.Router();
 
-router.get("/:shortCode", redirectShortUrl);
+router.get("/:shortCode", redirectLimiter, redirectShortUrl);
 
 export default router
--- src/routes/url.routes.js
+++ src/routes/url.routes.js
@@ -2,10 +2,10 @@ import express from "express";
 import protect from "../middleware/auth.middleware.js";
 import { createShortUrl, getUserUrls, getUrlCount } from "../controllers/url.controller.js";
 import { deleteUrl } from "../controllers/url.controller.js";
-
+import { createUrlLimiter } from "../middleware/rateLimit.middleware.js"
 const router = express.Router();
 
-router.post("/", protect, createShortUrl)
+router.post("/", protect, createUrlLimiter, createShortUrl)
 router.get("/", protect, getUserUrls)
 router.get("/count", protect, getUrlCount)
 router.delete("/:id", protect, deleteUrl)
